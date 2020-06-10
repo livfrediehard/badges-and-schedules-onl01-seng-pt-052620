4775ede969ae7e29b94f4a70f5acfbd7c1830fd7
Brian Palmer <bpalmer710@gmail.com>
2020-06-10 14:42:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,8 @@
  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
  
 def batch_badge_creator(array)
-   
+   attendees.each do |name|
+     return "Hello, my name is #{name}."
      
    end
  end
